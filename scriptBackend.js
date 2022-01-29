caed9749178ea2f4ef8ca69c4694754ed602cadf
Yuriy <dementev.yuriy@list.ru>
2022-01-29 13:02:04 +0300

    Рефакторинг

--- scriptBackend.js
+++ scriptBackend.js
@@ -1,28 +1,25 @@
-const requestURL = 'http://localhost:8080/api/users';
+const baseURL = 'http://localhost:8080/api';
+const getUsersURL = baseURL + '/users';
 
-function getUsers(type) {
-  const client = new XMLHttpRequest();
-  client.open('GET',requestURL);
-  client.responseType = type;
-  // client.setRequestHeader('Content-Type','application/json');
-  client.onload = () => {
-    if(client.status >= 200 && client.status <= 300) {
-      let users = client.response;
-      console.log(users);
-      for(let i = 0; i < users.length; i++) {
-        console.log(users[i].address.street);
-        //  передать в выподающий список
-        // addOption(usersCheckList[i]);
-        usersCheckList.innerHTML += `<select id="usersCheckList">
+async function initCheckListUsers() {
+  let users =  await doRequest(getUsersURL);
+  if (users != null) {
+    for (let i = 0; i < users.length; i++) {
+      //  передать в выподающий список
+      usersCheckList.innerHTML += `<select id="usersCheckList">
         <option value="">${users[i].name}</option>
     </select>`
-      }
-    }else {
-      console.error('Произошла ошибка. статус = ' + client.status);
     }
   }
-  client.send();
 }
 
-getUsers('json');
+async function doRequest( url)  {
+  let response = await fetch(url);
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return null;
+  }
+}
 
+initCheckListUsers();
